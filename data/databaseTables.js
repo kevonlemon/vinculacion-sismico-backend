ee7b87221ed6d2aae1e38493d5863ff8ed496390
yordanvc <yordan.2511@gmail.com>
2025-08-25 21:51:00 -0500

    feat: register users module

--- data/databaseTables.js
+++ data/databaseTables.js
@@ -2,9 +2,31 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const schema = process.env.DB_TEST_SCHEMA;
-const nombreTabla = `${schema}.nombreTabla`;
+
+const usuarios = `${schema}.usuarios`;
+const tokensRecuperacion = `${schema}.tokens_recuperacion`;
+const catalogos = `${schema}.catalogos`;
+const edificios = `${schema}.edificios`;
+const archivosEdificio = `${schema}.archivos_edificio`;
+const inspecciones = `${schema}.inspecciones`;
+const archivosInspeccion = `${schema}.archivos_inspeccion`;
+const comentariosInspeccion = `${schema}.comentarios_inspeccion`;
+const matrizPuntuacion = `${schema}.matriz_puntuacion`;
+const resultadosInspeccion = `${schema}.resultados_inspeccion`;
+const historialAuditoria = `${schema}.historial_auditoria`;
 
 const DatabaseTable = {
-  nombreTabla,
+  usuarios,
+  tokensRecuperacion,
+  catalogos,
+  edificios,
+  archivosEdificio,
+  inspecciones,
+  archivosInspeccion,
+  comentariosInspeccion,
+  matrizPuntuacion,
+  resultadosInspeccion,
+  historialAuditoria,
 };
+
 export default DatabaseTable;
